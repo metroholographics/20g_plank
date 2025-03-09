0ec638dae8c33dcef0cb85eec5b323fdc7d950ca
ali raja <nirvaanraja@gmail.com>
2025-03-09 18:48:24 +0000

    added sprites

--- main.c
+++ main.c
@@ -13,7 +13,10 @@
 #define C_GREY  (Color) {147, 163, 153, 255}
 
 #define PLANK_W GAME_WIDTH * 0.2f
-#define PLAYER_SIZE 30
+#define PLAYER_SIZE 64
+#define PLAYER_SPRITE_SIZE 32
+#define WATER_SPRITE_SIZE 32
+#define WATER_TILE_SIZE 128
 #define CANNON_RADIUS 35
 #define BULLET_RADIUS 5
 
@@ -45,7 +48,17 @@ typedef enum {
     IN_GAME
 } GameState;
 
+typedef struct anim_handler {
+    int num_frames;
+    float timer;
+    float anim_speed;
+    int current_frame;
+} AnimationHandler;
+
 typedef struct Player {
+    Rectangle source_rect;
+    Rectangle dest_rect;
+    AnimationHandler animation;
     Vector2 position;
     Color color;
     bool alive;
@@ -78,16 +91,10 @@ Rectangle plank_rect = {
     GAME_HEIGHT
 };
 
-Rectangle player_rect = {
-    GAME_WIDTH * 0.5f - (PLAYER_SIZE * 0.5f),
-    GAME_HEIGHT - PLAYER_SIZE,
-    PLAYER_SIZE,
-    PLAYER_SIZE
-};
-
 GameState game_state;
 Player player;
 Cannons cannons;
+Texture2D spritesheet;
 
 void update_player(void);
 void update_cannons(void);
@@ -101,43 +108,23 @@ int main(int argc, char* argv[])
     if (GetMonitorCount() > 0) SetWindowMonitor(1);
     else SetWindowMonitor(0);
 
+    spritesheet = LoadTexture("assets\\spritesheet.png");
+    if (!IsTextureValid(spritesheet)) {
+        printf("Couldn't load spritesheet\n");
+        CloseWindow();
+        return -1;
+    }
+
     SetRandomSeed((unsigned int) time(NULL));
 
     SetTargetFPS(120);
 
     game_state = IN_GAME;
 
-    player.position.x = player_rect.x;
-    player.position.y = player_rect.y;
-    player.color = (Color) {255, 255, 255, 255};
-    player.alive = true;
-
-    for (int i = 0; i < MAX_CANNONS; i++) {
-        float x, y;
-        x = 100;
-        y = 100 + (250 * i); 
-        if (i >= RIGHT_TOP) {
-            x = 1100;
-            y = 100 + (250 * (i % 3));
-        }
-        cannons.positions[i] = (Vector2) {x,y};
-        cannons.bullet[i] = (BulletHandler) {
-            .bullet_position = {0,0},
-            .lock_on = {0,0},
-            .timer = 0.0f,
-            .state = IDLE
-        };
-        cannons.movement[i] = (MovementHandler) {
-            .centre_pos = (Vector2) {x, y},
-            .target_pos = (Vector2) {x, y},
-            .timer = 0.0f,
-            .state = STATIONARY
-        };
-    }
-
+    reset_game();
+    
     while (!WindowShouldClose()) 
     {
-
         {
             if (!player.alive) game_state = RESET_STATE;
              
@@ -152,8 +139,6 @@ int main(int argc, char* argv[])
             }
 
             if (game_state == MAIN_MENU) {
-                
-                
                 if (IsKeyPressed(KEY_SPACE)) {
                     game_state = IN_GAME;
                 }
@@ -162,10 +147,43 @@ int main(int argc, char* argv[])
                     
         BeginDrawing();
             ClearBackground(C_BLUE);
-            DrawRectangleRec(plank_rect, C_BROWN);
+            // DrawRectangleRec(plank_rect, C_BROWN);
+            {
+                static float tile_timer = 0.0f;
+                static int index = 0;
+                tile_timer += GetFrameTime();
+                if (tile_timer > 0.2) {
+                    index++;
+                    tile_timer = 0.0f;
+                    if (index == 3) {
+                        index = 0;
+                    }
+                }
+                Rectangle tile_source = (Rectangle) {index * WATER_SPRITE_SIZE, 200, WATER_SPRITE_SIZE, WATER_SPRITE_SIZE};
+                for (int y = 0; y < (GAME_HEIGHT + WATER_TILE_SIZE) / WATER_TILE_SIZE; y++) {
+                    for (int x = 0; x < (GAME_WIDTH + WATER_TILE_SIZE) / WATER_TILE_SIZE; x++) {
+                        Rectangle dest_tile = (Rectangle) {x*WATER_TILE_SIZE,y*WATER_TILE_SIZE,WATER_TILE_SIZE,WATER_TILE_SIZE};
+                        DrawTexturePro(spritesheet, tile_source, dest_tile,(Vector2){0,0}, 0.0f, WHITE);
+                    }
+                }    
+            }
+            
+            DrawTexturePro(
+                spritesheet,
+                (Rectangle){0, 64,48, 136},
+                plank_rect,
+                (Vector2){0,0}, 0.0f, WHITE
+            );
+
             for (int i = 0; i < MAX_CANNONS; i++) {
                 Vector2 can_pos = cannons.positions[i];
-                DrawCircleV(can_pos, CANNON_RADIUS, C_BLACK);
+                // DrawCircleV(can_pos, CANNON_RADIUS, C_BLACK);
+                int flip = (i < RIGHT_TOP) ? 1 : -1;
+                DrawTexturePro(spritesheet,
+                    (Rectangle) {0,232,flip*32,32},
+                    (Rectangle) {can_pos.x, can_pos.y, 96, 96},
+                    (Vector2) {16,16}, 0.0f, WHITE
+                 );
             }
             for (int i = 0; i < MAX_CANNONS; i++) {
                 BulletHandler b = cannons.bullet[i];
@@ -173,18 +191,23 @@ int main(int argc, char* argv[])
                     DrawLineEx(cannons.positions[i], b.lock_on, 2.0f, C_GREY); 
                 }
                 if (b.state == FIRING) {
-                    DrawLineEx(b.bullet_position, b.lock_on, 1.5f, C_RED);
-                    DrawCircleV(b.bullet_position, BULLET_RADIUS, C_BLACK);
+                    // DrawLineEx(b.bullet_position, b.lock_on, 1.5f, C_RED);
+                    // DrawCircleV(b.bullet_position, BULLET_RADIUS, C_BLACK);
+                    DrawTexturePro(spritesheet, (Rectangle){48,64,32,32},
+                                   (Rectangle){b.bullet_position.x,b.bullet_position.y,32,32},
+                               (Vector2){4,4}, 0, WHITE);
                 }
             }
-            DrawRectangleRec(player_rect, player.color);
+            DrawTexturePro(spritesheet, player.source_rect, player.dest_rect, (Vector2) {0,0}, 0, WHITE);
             if (game_state == MAIN_MENU) {
                 DrawText("Press Space to play", GAME_WIDTH*0.5f, GAME_HEIGHT*0.5f, 22, C_BLACK);
             }
+            DrawFPS(20,20);
         EndDrawing();
     }
     
      
+
     
     CloseWindow();
     return 0;
@@ -192,17 +215,24 @@ int main(int argc, char* argv[])
 
 void reset_game(void) {
     
-    player_rect = (Rectangle) {
+    player.dest_rect = (Rectangle) {
         GAME_WIDTH * 0.5f - (PLAYER_SIZE * 0.5f),
         GAME_HEIGHT - PLAYER_SIZE,
         PLAYER_SIZE,
         PLAYER_SIZE
     };
-    
-    player.position.x = player_rect.x;
-    player.position.y = player_rect.y;
+    player.source_rect.width = player.source_rect.height = PLAYER_SPRITE_SIZE;
+    player.source_rect.y = 32; 
+    player.position.x = 0;
+    player.position.y = 0;
     player.color = (Color) {255, 255, 255, 255};
     player.alive = true;
+    player.animation = (AnimationHandler) {
+        .num_frames = 2,
+        .timer = 0.0f,
+        .current_frame = 0,
+        .anim_speed = 0.15f,
+    };
 
     for (int i = 0; i < MAX_CANNONS; i++) {
         float x, y;
@@ -233,32 +263,51 @@ void reset_game(void) {
 void update_player(void)
 {
     float dt = GetFrameTime();
+
+    player.position.x = 0;
+    player.position.y = 0;
+    
     if (IsKeyDown(KEY_UP)) {
-        player.position.y -= 100 * dt;     
+        player.position.y = -100 * dt;     
     }
     if (IsKeyDown(KEY_DOWN)) {
-        player.position.y += 100 * dt;
+        player.position.y = 100 * dt;
     }
     if (IsKeyDown(KEY_LEFT)) {
-        player.position.x -= 100 * dt;
+        player.position.x = -100 * dt;
     }
     if (IsKeyDown(KEY_RIGHT)) {
-        player.position.x += 100 * dt;
+        player.position.x = 100 * dt;
     }
-
-    if (player.position.y + PLAYER_SIZE > GAME_HEIGHT) player.position.y = GAME_HEIGHT - PLAYER_SIZE;
     
-    if (player.position.x + (0.5f * PLAYER_SIZE) < plank_rect.x || player.position.x + 0.5f * PLAYER_SIZE > plank_rect.x + PLANK_W) {
-        player.alive = false;
-        player.color = (Color) {220, 100, 100 , 255};
-    } else {
-        player.alive = true;
-    }
-
     player.color = (player.alive) ? (Color) {255, 255, 255, 255} : (Color) {220, 100, 100, 255};
 
-    player_rect.x = player.position.x;
-    player_rect.y = player.position.y;
+    player.dest_rect.x += player.position.x;
+    player.dest_rect.y += player.position.y;
+
+    if (player.dest_rect.y + PLAYER_SIZE > GAME_HEIGHT) player.dest_rect.y = GAME_HEIGHT - PLAYER_SIZE;
+    if (player.dest_rect.x + (0.5f * PLAYER_SIZE) < plank_rect.x || player.dest_rect.x + 0.5f * PLAYER_SIZE > plank_rect.x + PLANK_W) {
+        player.alive = false;
+        player.color = (Color) {220, 100, 100 , 255};
+    }
+
+    bool moving = (player.position.x != 0.0f || player.position.y != 0.0f);
+    
+    if (moving) {
+        player.animation.timer += dt;
+        if (player.animation.timer > player.animation.anim_speed) {
+            player.animation.current_frame += 1;
+            player.animation.timer = 0.0f;
+            if (player.animation.current_frame == player.animation.num_frames) {
+                player.animation.current_frame = 0;
+            }
+        }     
+    } else {
+        player.animation.current_frame = 0;
+    }
+    player.source_rect.x = player.animation.current_frame * PLAYER_SPRITE_SIZE;
+    if (player.position.y < 0) player.source_rect.y = 1 * PLAYER_SPRITE_SIZE;
+    else if (player.position.y > 0) player.source_rect.y = 0 * PLAYER_SPRITE_SIZE;
 }
 
 void update_cannons(void)
@@ -286,7 +335,6 @@ void update_cannons(void)
                 } else if (Vector2Equals(cannons.positions[i], m->target_pos) && !Vector2Equals(m->target_pos, m->centre_pos)) {
                     m->target_pos = m->centre_pos;
                 } else if (Vector2Equals(cannons.positions[i], m->centre_pos)) {
-                    printf("stopping\n");
                     m->target_pos = (Vector2) {0,0};
                     m->state = STATIONARY;
                     m->timer = 0.0f;
@@ -306,7 +354,7 @@ void update_cannons(void)
         BulletHandler* b = &cannons.bullet[i];
         b->timer += dt;
         
-        if (b->timer >= 1.25 && b->state == IDLE) {
+        if (b->timer >= 1.0f && b->state == IDLE) {
             int chance = GetRandomValue(1, 10);
             if (chance <= 1) {
                 b->state = LOCKING_ON;
@@ -316,7 +364,7 @@ void update_cannons(void)
         }
         if (b->state == LOCKING_ON) {
             if (b->timer < 1.0f) {
-                b->lock_on = (Vector2) {player.position.x + 0.5f*PLAYER_SIZE, player.position.y + 0.5f*PLAYER_SIZE};
+                b->lock_on = (Vector2) {player.dest_rect.x + 0.5f*PLAYER_SIZE, player.dest_rect.y + 0.5f*PLAYER_SIZE};
             } else {
                 b->state = FIRING;
                 b->timer = 0.0f;
@@ -324,7 +372,8 @@ void update_cannons(void)
         }
         if (b->state == FIRING) {
             b->bullet_position = Vector2MoveTowards(b->bullet_position, b->lock_on, 200 * dt);
-            bool hit_player = CheckCollisionCircleRec(b->bullet_position, BULLET_RADIUS, player_rect);
+            //::todo: learn how to do circle->rect collision myself
+            bool hit_player = CheckCollisionCircleRec(b->bullet_position, BULLET_RADIUS, player.dest_rect);
             if (Vector2Equals(b->bullet_position, b->lock_on) || hit_player) {
                 b->state = IDLE;
                 b->timer = 0.0f;
